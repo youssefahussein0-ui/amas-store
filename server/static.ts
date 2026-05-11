8f9f7b220ec31d9a30fc77f49484b7ac8a915711
Amas Store <amas@amas-store.com>
2026-05-11 18:31:31 +0300

    Use middleware catch-all for Express 5

--- server/static.ts
+++ server/static.ts
@@ -13,7 +13,7 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.get("/:path*", (_req, res) => {
+  app.use((_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
